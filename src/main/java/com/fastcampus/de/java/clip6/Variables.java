8554952511833bd7160c57adaf543da4ec12a370
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 <ngoodsamari@naver.com>
2023-05-23 00:34:01 +0900

    chracter, number default

---
+++ src/main/java/com/fastcampus/de/java/clip6/Variables.java
@@ -0,0 +1,11 @@
+package com.fastcampus.de.java.clip6;
+
+public class Variables {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+
+        String str = "hello world";
+        System.out.println(str);
+    }
+}
