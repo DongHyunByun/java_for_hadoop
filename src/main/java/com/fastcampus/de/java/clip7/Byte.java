bcdc0ae0f563c40e9c31709b0ee4f97839666d8e
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 <ngoodsamari@naver.com>
2023-05-23 23:31:15 +0900

    stopped by cty dh

---
+++ src/main/java/com/fastcampus/de/java/clip7/Byte.java
@@ -0,0 +1,8 @@
+package com.fastcampus.de.java.clip7;
+
+public class Byte {
+    public static void main(String[] args) {
+        byte b = 'd'; //ㅇd의 아스키코드 값이 들어간다
+        System.out.println(b);
+    }
+}
