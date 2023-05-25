9a3b3e371e40ff538e307adbb0c784102b1a184b
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 <ngoodsamari@naver.com>
2023-05-26 01:03:50 +0900

    for, while

---
+++ src/main/java/com/fastcampus/de/java/clip11_1/Main.java
@@ -0,0 +1,19 @@
+package com.fastcampus.de.java.clip11_1;
+
+public class Main {
+    static class Person {
+        String name;
+        String country;
+        int age;
+    }
+
+    public static void main(String[] args) {
+        Person minsoo =  new Person();
+        minsoo.name = "민수";
+        minsoo.country = "대한민국";
+        minsoo.age = 10;
+
+        Person paul = new Person();
+
+    }
+}
