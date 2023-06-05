f980d75c18b30c3eb961ffcb5424803434c3da72
Byun Dong Hyun <ngoodsamari@naver.com>
2023-06-05 23:16:53 +0900

    clip 17

---
+++ src/com/fastcampus/de/java/clip17/StreamExample4.java
@@ -0,0 +1,26 @@
+package com.fastcampus.de.java.clip17;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExample4 {
+    public static void main(String[] args) {
+        List<Integer> numArr = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        System.out.println("Integer::Sum");
+        int result = numArr.stream()
+                .reduce(0,Integer::sum);
+        System.out.println(result);
+
+        System.out.println("streamExample4::Sum");
+        int result2 = numArr.stream()
+                .reduce(0,StreamExample4::sum);
+        System.out.println(result2);
+    }
+
+    public static int sum(int a,int b) {
+        System.out.println("a = "+a+","+"b = "+b);
+        return a+b;
+    }
+
+}
